d751623861c68483cb8d01a3a0303535f2e8f961
kang seok <sekeipost@naver.com>
2023-01-04 14:41:51 +0900

    refactor : refactor the data's interfaces

--- src/interface/keyword/index.ts
+++ src/interface/keyword/index.ts
@@ -1,6 +1,11 @@
 import { ObjectId } from "mongoose";
 
-export enum categories {}
+export enum categories {
+  human = "인물",
+  politics = "정치",
+  policy = "정책",
+  economy = "경제",
+}
 
 export interface KeywordInf {
   keyword: String;
