6ecb7584547a53bb4b0ff8adbe9dff57fee023f6
kang seok <sekeipost@naver.com>
2024-02-02 07:35:55 +0900

    add the refresh api doc

--- src/Api/v1/Auth.js
+++ src/Api/v1/Auth.js
@@ -39,3 +39,32 @@
             }
         }
  */
+
+/**
+ * @api {post} /auth/refresh Refresh access token
+ * 
+ * @apiVersion        1.0.0
+ * @apiName RefreshAccess
+ * @apiGroup Auth
+ *
+ * @apiHeader {String} Authorization='Bearer ${string}'
+ *
+ * @apiSuccessExample Success-Response:
+ *      HTTP/1.1 200 OK
+ *      {
+            success : boolean, 
+            result : {
+                access : string
+            }
+        }
+ *
+ * @apiError (401 NoAuthorization) AuthorizationTokenNeeded
+ * @apiErrorExample {json} Error-Response:
+       HTTP/1.1 500 Unauthorized
+ *     {
+ *       "success" : false,
+ *       "result" : {
+ *         "error" : "The authorization token is needed"   
+ *       }
+ *     }
+ */
