e139bdc12b33b550ba28a62d3b4ed2e28fc3d162
kang seok <sekeipost@naver.com>
2024-10-10 23:55:33 +0900

    end convert keyword explain

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import express, { Request, Response } from "express";
 import fs from "fs";
 import path from "path";
 
-import { explainToHtml } from "./mig";
 import { authRoute } from "./routes/auth";
 import { keywordRoute } from "./routes/keywords";
 import { keywordAdminRoute } from "./routes/keywordsAdmin";
@@ -93,6 +92,6 @@ app.listen(app.get("port"), () => {
 // fixTheKeywords();
 //migrateNewsIsPublished();
 
-explainToHtml();
+//explainToHtml();
 
 //summaryToHtml();
